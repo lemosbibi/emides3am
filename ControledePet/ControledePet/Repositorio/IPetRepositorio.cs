bbe87e69be6cf9a59ec988f7cb39e1705fcd3640
Bianca Lemos <bianca.saraiva@aluno.senai.br>
2023-08-25 16:11:06 -0300

    CRUD

---
+++ ControledePet/ControledePet/Repositorio/IPetRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using ControledePet.Models;
+
+namespace ControledePet.Repositorio
+{
+    public interface IPetRepositorio
+    {
+        List<PetModel> BuscarTodos();
+        PetModel Adicionar(PetModel pet);
+        PetModel Atualizar(PetModel pet);
+        
+    }
+}
